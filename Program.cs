11c939040eaebe29b7414e9f3f9719b0e49855b2
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2023-11-13 16:38:25 +0530

    Assign2MethParametersDay6

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign2MethParametersDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OurClass Hr = new OurClass();
+            Hr.display(workingHours: 8, workingdays: 30);
+            OurClass Admin = new OurClass();
+            Admin.display(workingHours: 8, workingdays: 30, projecthandel: 3);
+            OurClass softwareDeveloper = new OurClass();
+            softwareDeveloper.display(workingHours: 8, workingdays: 30, projecthandel: 4, extra: 5);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assign2MethParametersDay6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assign2MethParametersDay6")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa443f51-1c95-4d12-8e2f-e90b21f212e8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
